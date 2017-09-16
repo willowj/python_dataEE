e5a7ee9c905b9804e14e92c4a4f538fcee4a08dc
willowj <765755345@qq.com>
2017-09-16 22:51:05 +0800

    Update kline_candle.py

--- plot/kline_candle.py
+++ plot/kline_candle.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+  
+  
+
+
+
+
+# jupyter-notebook
+# author : willow_j
+# email : willowsophy@foxmail.com
 
 from __future__ import unicode_literals
 from __future__ import print_function
@@ -7,28 +16,35 @@ from __future__ import print_function
 from pyecharts import Kline, Bar, Line, Grid, Overlap
 
 
-def kline_js(name, df, prices_cols=None, ma=('ma10',),width=1600, height=750,kline_xaxis_pos='top', render_path=None):
-    # prices_cols = [u'open', u'close', u'low', u'high'
-    # othercoks = ['volume']
-    # candle
+def kline_js(name, df, prices_cols=None, ma=('ma10',), width=1600, height=750, kline_xaxis_pos='top', render_path=None):
+    '''
+    params:
+    name:    图例名称
+    df:    columns包含 prices_cols以及‘volume’ 的pandas.DataFrame
+    prices_cols :  默认 [u'open', u'close', u'low', u'high']
+    width=1600, height=750 :   默认图片大小
+    kline_xaxis_pos='top'： k-line图例默认在上方
+    render_path：   render to html file path
+    '''
     if not prices_cols:
         prices_cols = [u'open', u'close', u'low', u'high']
 
     if not set(prices_cols+['volume']) < set(df.columns):
-        raise AttributeError("%s or 'volume' not in columns"%str(prices_cols))
+        raise AttributeError("%s or 'volume' not in columns" %
+                             str(prices_cols))
 
     df.sort_index(inplace=True)
-    kline = Kline(name,width=width, height=height)
-    kline.add('k-candle', 
-        df.index.format(),
+    kline = Kline(name, width=width, height=height)
+    kline.add('k-candle',
+              df.index.format(),
               df[prices_cols].values.tolist(),
               is_datazoom_show=True,
               datazoom_xaxis_index=[0, 1],
               xaxis_pos=kline_xaxis_pos,
               is_xaxislabel_align=True,
-             )
+              )
 
-     # volume   
+    # volume
     if not 'price_change' in df.columns:
         df['price_change'] = df[prices_cols[1]].diff()
     ups = df.where(df.price_change > 0, 0)['volume']
@@ -42,20 +58,20 @@ def kline_js(name, df, prices_cols=None, ma=('ma10',),width=1600, height=750,kli
             legend_top="70%",
             is_stack=True,
             is_xaxislabel_align=True,
-          
-           )
+
+            )
     bar.add('down',
             x_axis=downs.index.format(),
-            y_axis = downs.values.tolist(),
+            y_axis=downs.values.tolist(),
             is_datazoom_show=True,
             is_stack=True,
             legend_top="70%",
-            legend_orient = 'vertical',
+            legend_orient='vertical',
             legend_pos='left',
-           yaxis_pos='right',
-           is_xaxislabel_align =True,
-            #mark_line=["average"],
-           )
+            yaxis_pos='right',
+            is_xaxislabel_align=True,
+            # mark_line=["average"],
+            )
 
     # merge
     grid1 = Grid()
@@ -66,8 +82,8 @@ def kline_js(name, df, prices_cols=None, ma=('ma10',),width=1600, height=750,kli
     Line_draw = False
     for ma_ in ma:
         if ma_ in df.columns:
-            if  Line_draw is False:
-                Line_draw = True       
+            if Line_draw is False:
+                Line_draw = True
                 line = Line()
             line.add(ma_, df.index.format(), df[ma_].values.tolist())
     if Line_draw:
@@ -81,10 +97,10 @@ def kline_js(name, df, prices_cols=None, ma=('ma10',),width=1600, height=750,kli
 
 if __name__ == '__main__':
     import tushare as ts
-    name ='hs300'
+    name = 'hs300'
     period = '5'
     df = ts.get_hist_data(name, ktype=period)
     if period.isdigit():
-        period +='min'
-    t = kline_js('%s_kline_%s'%(name,period), df, ma=['ma10','ma20'])
-    t.render('%s_kline_%s.html'%(name,period)) 
+        period += 'min'
+    t = kline_js('%s_kline_%s' % (name, period), df, ma=['ma10', 'ma20'])
+    t.render('%s_kline_%s.html' % (name, period))
